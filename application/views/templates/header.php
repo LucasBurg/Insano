71cff0c44a064978f58a194a13d3a47d454f7fa3
Lucas Daniel Burg Mota <lucas.mota@ielusc.br>
2017-10-02 08:24:50 -0300

    Troquei a versão do framework css

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="<?= site_url('assets\bootstrap-4.0.0\css\bootstrap.min.css') ?>">
+        <link rel="stylesheet" href="<?= site_url('assets\bootstrap-3.3.7\css\bootstrap.min.css') ?>">
     </head>    
-<body>
+    <body>
 
