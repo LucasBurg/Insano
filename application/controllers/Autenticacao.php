846c0c3cb6bed3f946bbeb015dfa5cf553e49f96
Lucas Daniel Burg Mota <lucas.mota@ielusc.br>
2017-10-02 13:42:29 -0300

    Controle Autenticação, vamos separar as ações de entrar e sair

---
+++ application/controllers/Autenticacao.php
@@ -0,0 +1,23 @@
+<?php
+class Autenticacao extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function index()
+    {
+        show_404();
+    }
+
+    public function entrar() 
+    {
+        
+    }
+    
+    public function sair()
+    {
+        
+    }
+}
